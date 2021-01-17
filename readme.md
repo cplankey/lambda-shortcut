b1d26770f80ca360a4e2ab25e514772d800a399b
Chris Plankey <plankey5@aol.com>
2021-01-17 15:24:33 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Lambda Shortcut
+
+Lambda Shortcut is a Google Chrome extension that creates a button in the AWS CloudWatch UI when you are in Lambda logs to navigate to your Lambda function.
+
+This has no affiliation with AWS or Amazon.
+
+## Installation
+
+Until Google accepts my app submission, you'll need to zip up this repo and follow [these steps](https://developer.chrome.com/docs/extensions/mv2/getstarted/)
+
+![example](./demo.png)
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
---
+++ script.js
@@ -0,0 +1,19 @@
+if (document.getElementById('openFunctionExtension')){
+    document.getElementById('openFunctionExtension').remove()
+}
+if (window.location.href.indexOf('https://console.aws.amazon.com/cloudwatch/home') !== -1 && window.location.href.indexOf('log-groups/log-group/$252Faws$252Flambda$252F') !== -1){
+    //in lambda logs
+    let link = document.createElement("a");
+    link.innerHTML = "Open Function"
+    link.id = "openFunctionExtension"
+    link.target ="_blank"
+    link.style = "color:white; background:#ec7211; font-weight:bold"
+    let str = window.location.href;
+    let region = str.substr(str.indexOf('region=')+7, str.indexOf('#')-str.indexOf('region=')-7)
+    let functionName = str.substr(str.indexOf('$252Faws$252Flambda$252F')+24);
+    functionName.indexOf('/log-events/') !== -1 ? functionName = functionName.substr(0,functionName.indexOf('/log-events')): functionName = functionName;
+    link.href = `https://console.aws.amazon.com/lambda/home?region=${region}#/functions/${functionName}?tab=configuration`
+    document.getElementById('awsc-nav-header').appendChild(link)
+} else{
+    //nothing to see here
+}
